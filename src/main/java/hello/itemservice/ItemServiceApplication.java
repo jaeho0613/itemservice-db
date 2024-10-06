8c0be30810509e442e6a8038b5cf0b157bb90c35
jaeho0613 <ywm2004@gmail.com>
2024-10-06 22:12:07 +0900

    섹션5 - 데이터 접근 기술 MyBatis
    - Mybatis 적용3 - 분석까지

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -17,7 +17,8 @@ import javax.sql.DataSource;
 // @Import(MemoryConfig.class)
 // @Import(JdbcTemplateV1Config.class)
 // @Import(JdbcTemplateV2Config.class)
-@Import(JdbcTemplateV3Config.class)
+// @Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
@@ -31,17 +32,17 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
     
-    // @Bean
-    // @Profile("test")
-    // public DataSource dataSource() {
-    //     log.info("메모리 데이터베이스 초기화");
-    //     DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    //     dataSource.setDriverClassName("org.h2.Driver");
-    //     dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-    //     dataSource.setUsername("sa");
-    //     dataSource.setPassword("");
-    //    
-    //     return dataSource;
-    // }
+    /*@Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        log.info("메모리 데이터베이스 초기화");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }*/
 
 }
