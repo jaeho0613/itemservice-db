5af8f9dea9e20610425501f4f3f82d0c729e9ee4
jaeho0613 <ywm2004@gmail.com>
2024-10-08 00:17:42 +0900

    섹션7 완료

---
+++ src/main/java/hello/itemservice/config/SpringDataJpaConfig.java
@@ -0,0 +1,27 @@
+package hello.itemservice.config;
+
+import hello.itemservice.jpa.JpaItemRepositoryV2;
+import hello.itemservice.jpa.SpringDataJpaItemRepository;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SpringDataJpaConfig {
+
+    private final SpringDataJpaItemRepository springDataJpaItemRepository;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JpaItemRepositoryV2(springDataJpaItemRepository);
+    }
+}
